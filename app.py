fc2db590607af266d994d1f6eaba65b8436f599e
Arpittgaikwad29 <158511811+Arpittgaikwad29@users.noreply.github.com>
2025-04-23 02:24:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,6 @@ def Predict():
         else:
             return "Error: No coordinates provided. Please select a place."
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
